28298a33c9d9ab6d5c6c30e869cf07492e216835
Badr-Eddine El Mostaine <100159431+Elmostaine95@users.noreply.github.com>
2023-07-16 20:46:52 +0100

    create services index

--- backend/E-shop/src/services/index.ts
+++ backend/E-shop/src/services/index.ts
@@ -1,6 +1,24 @@
-import {  } from "./";
+import { address_Service } from "./concrete/address_Service";
+import { cart_item_Service } from "./concrete/cart_item_service";
+import { category_item_Service } from "./concrete/category_item_service";
+import { category_Service } from "./concrete/category_service";
+import { item_photo_video_Service } from "./concrete/item_photo_video_service";
+import { item_Service } from "./concrete/item_service";
+import { order_item_Service } from "./concrete/order_item_service";
+import { order_Service } from "./concrete/order_service";
+import { store_Service } from "./concrete/store_service";
+import { wichlist_item_Service } from "./concrete/wichlist_item_service";
 
 
 export {
-
+    address_Service,
+    cart_item_Service,
+    category_item_Service,
+    category_Service,
+    item_photo_video_Service,
+    item_Service,
+    order_item_Service,
+    order_Service,
+    store_Service,
+    wichlist_item_Service
 }
